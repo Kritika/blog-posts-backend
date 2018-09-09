9d1f5a8470d94a5619b062773193df2eceb0d754
Kritika <kritika28@icloud.com>
2018-09-09 21:46:07 +0530

    First commit

---
+++ src/main/java/com/blog/blogpostsbackend/BlogPostsBackendApplication.java
@@ -0,0 +1,12 @@
+package com.blog.blogpostsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPostsBackendApplication.class, args);
+	}
+}
